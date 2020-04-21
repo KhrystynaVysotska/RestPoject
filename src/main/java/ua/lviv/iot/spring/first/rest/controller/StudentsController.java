c5dcab762da00460d8b090b688f6be734fe8dc79
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-04-21 10:45:16 +0300

    Provided rest service for StudentController to use crud methods from service

--- src/main/java/ua/lviv/iot/spring/first/rest/controller/StudentsController.java
+++ src/main/java/ua/lviv/iot/spring/first/rest/controller/StudentsController.java
@@ -1,10 +1,6 @@
 package ua.lviv.iot.spring.first.rest.controller;
 
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,42 +22,46 @@ import ua.lviv.iot.spring.first.rest.model.Student;
 @RestController
 public class StudentsController {
 
-	@Autowired
-	private StudentService studentService;
-	
-	@GetMapping
-	public List<Student> getStudents(final @RequestParam(value = "name", required = false) String name) {
-		if (name == null) {
-			return studentService.findAll();
-		}
-		return studentService.getAllByName(name);
-	}
+    @Autowired
+    private StudentService studentService;
 
-	@GetMapping(path = "/{id}")
-	public Student getStudent(final @PathVariable("id") Integer studentId) {
-		return students.get(studentId);
-	}
+    @GetMapping
+    public List<Student> getStudents(final @RequestParam(value = "name", required = false) String name) {
+        if (name == null) {
+            return studentService.findAll();
+        }
+        return studentService.getAllByName(name);
+    }
 
-	@PostMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
-	public Student createStudent(final @RequestBody Student student) {
-		return studentService.createStudent(student);
+    @GetMapping(path = "/{id}")
+    public Student getStudent(final @PathVariable("id") Integer studentId) {
+        return studentService.getStudent(studentId);
+    }
 
-	}
+    @PostMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
+    public Student createStudent(final @RequestBody Student student) {
+        return studentService.createStudent(student);
 
-	@DeleteMapping(path = "/{id}")
-	public ResponseEntity<Student> deleteStudent(@PathVariable("id") Integer studentId) {
+    }
 
-		HttpStatus status = students.remove(studentId) == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
-		return ResponseEntity.status(status).build();
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Student> deleteStudent(@PathVariable("id") Integer studentId) {
+        if (studentService.deleteById(studentId)) {
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 
-	}
-
-	@PutMapping(path = "/{id}")
-	public ResponseEntity<Student> updateStudent(final @PathVariable("id") Integer studentId,
-			@RequestBody Student student) {
-		student.setId(studentId);
-		HttpStatus status = students.put(studentId, student) != null ? HttpStatus.OK : HttpStatus.NOT_FOUND;
-		return ResponseEntity.status(status).build();
-
-	}
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Student> updateStudent(final @PathVariable("id") Integer studentId,
+            @RequestBody Student student) {
+        student.setId(studentId);
+        Student updatedStudent = studentService.update(studentId, student, new Student());
+        if (updatedStudent != null) {
+            return new ResponseEntity<Student>(updatedStudent, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<Student>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
