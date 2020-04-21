a31d3401ac6b3579235538ca11632b2abd77d1ca
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-04-21 10:44:32 +0300

    Fixed crud methods to be able to work with database , not to write to hashmap

--- src/main/java/ua/lviv/iot/spring/first/business/StudentService.java
+++ src/main/java/ua/lviv/iot/spring/first/business/StudentService.java
@@ -2,6 +2,7 @@ package ua.lviv.iot.spring.first.business;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,24 +11,42 @@ import ua.lviv.iot.spring.first.rest.model.Student;
 
 @Service
 public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
+    }
     
-	@Autowired
-	private StudentRepository studentReposetory;
+    public Student getStudent(Integer studentId) {
+        return studentRepository.findById(studentId).get();
+    }
 
-	public Student createStudent(Student student) {
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+    
+    public boolean deleteById(Integer id) {
+        if (studentRepository.existsById(id)) {
+            studentRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-		return studentReposetory.save(student);
+    public List<Student> getAllByName(String name) {
 
-	}
-
-	public List<Student> findAll() {
-
-		return studentReposetory.findAll();
-	}
-
-	public List<Student> getAllByName(String name) {
-
-		return studentReposetory.findAllByName(name);
-	}
+        return studentRepository.findAllByName(name);
+    }
+    
+    public Student update(Integer id, Student newStudent, Student oldStudent) {
+        if (studentRepository.existsById(id)) {
+            BeanUtils.copyProperties(studentRepository.findById(id).get(), oldStudent);
+            studentRepository.save(newStudent);
+        }
+        return oldStudent;
+    }
 
 }
