5fc7598b575de4783a302f2b0d37b67ae49e1f46
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-04-21 10:52:39 +0300

    Provided crud methods to work with database

--- src/main/java/ua/lviv/iot/spring/first/business/GroupService.java
+++ src/main/java/ua/lviv/iot/spring/first/business/GroupService.java
@@ -2,6 +2,7 @@ package ua.lviv.iot.spring.first.business;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +12,35 @@ import ua.lviv.iot.spring.first.rest.model.Group;
 @Service
 public class GroupService {
 
-	@Autowired
-	private GroupRepository groupRepository;
+    @Autowired
+    private GroupRepository groupRepository;
 
-	public List<Group> findAll() {
-		return groupRepository.findAll();
-	}
+    public List<Group> findAll() {
+        return groupRepository.findAll();
+    }
+
+    public Group createGroup(Group group) {
+        return groupRepository.save(group);
+    }
+
+    public Group getGroup(Integer groupId) {
+        return groupRepository.findById(groupId).get();
+    }
+
+    public boolean deleteById(Integer id) {
+        if (groupRepository.existsById(id)) {
+            groupRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Group update(Integer id, Group newGroup, Group oldGroup) {
+        if (groupRepository.existsById(id)) {
+            BeanUtils.copyProperties(groupRepository.findById(id).get(), oldGroup);
+            groupRepository.save(newGroup);
+        }
+        return oldGroup;
+    }
 }
