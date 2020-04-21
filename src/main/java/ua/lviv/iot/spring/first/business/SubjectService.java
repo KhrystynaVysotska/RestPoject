36b7a1c84241f1aab3f88bac45c01fc4600af9d6
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-04-21 11:04:03 +0300

    Provided crud methods to work with database

--- src/main/java/ua/lviv/iot/spring/first/business/SubjectService.java
+++ src/main/java/ua/lviv/iot/spring/first/business/SubjectService.java
@@ -2,9 +2,9 @@ package ua.lviv.iot.spring.first.business;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import ua.lviv.iot.spring.first.dataaccess.SubjectRepository;
 import ua.lviv.iot.spring.first.rest.model.Subject;
 
@@ -17,5 +17,29 @@ public class SubjectService {
 	public List<Subject> findAll() {
 		return subjectRepositiry.findAll();
 	}
+	
+	public Subject createSubject(Subject subject) {
+        return subjectRepositiry.save(subject);
+    }
 
+    public Subject getSubject(Integer subjectId) {
+        return subjectRepositiry.findById(subjectId).get();
+    }
+
+    public boolean deleteById(Integer id) {
+        if (subjectRepositiry.existsById(id)) {
+            subjectRepositiry.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Subject update(Integer id, Subject newSubject, Subject oldSubject) {
+        if (subjectRepositiry.existsById(id)) {
+            BeanUtils.copyProperties(subjectRepositiry.findById(id).get(), oldSubject);
+            subjectRepositiry.save(newSubject);
+        }
+        return oldSubject;
+    }
 }
